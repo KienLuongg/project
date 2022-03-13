0e7cca43edb66b0248defd41bddda8937430a039
KienLuongg <101387392+KienLuongg@users.noreply.github.com>
2022-03-13 15:19:38 +0700

    Add files via upload

---
+++ Char.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	char Kien[100];
+	ptintf("hey");
+	gets(Kien);
+	printf("Welcome %s to T2203M\n", Kien);
+	return 0;
+}
