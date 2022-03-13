75b233464eaba8dc90735d10453173135a9758be
KienLuongg <101387392+KienLuongg@users.noreply.github.com>
2022-03-13 15:22:53 +0700

    Add files via upload

---
+++ Vd11bai1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i;
+	int k=0;
+
+	for( i = 1; i < 98; i= i+2){
+		printf("%d\n", k);
+		scanf("%d", &k);
+		
+	
+		
+	} 
+	
+	
+	return 0;
+}
