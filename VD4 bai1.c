8f9f580d5ce5bb00c005af5ea90be8fde4fddabb
KienLuongg <101387392+KienLuongg@users.noreply.github.com>
2022-03-13 15:21:56 +0700

    Add files via upload

---
+++ VD4 bai1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int (num);
+	printf ("enter a number num =");
+	scanf("%d", &num);
+	if (num%2==0);
+	printf("number is even");
+	return 0;
+}
